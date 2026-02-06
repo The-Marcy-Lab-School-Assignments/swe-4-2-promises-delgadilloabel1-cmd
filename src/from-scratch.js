922e0cba858c9a261c87887ab0ab35919c788ca5
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2026-02-06 08:59:04 -0500

    pushing what i have

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,17 +1,26 @@
 const resolvedWrapper = (value) => {
+  return Promise.resolve(value);
 };
 
 const rejectedWrapper = (errorMessage) => {
+  let error = new Error(errorMessage);
+  const myPromise = new Promise((resolve, reject) => {
+    reject(error);
+  });
+  return myPromise;
+  // return Promise.reject(errorMessage);
 };
 
 const handleResolvedPromise = (promise) => {
+  return promise.then((message) => {
+    console.log(message);
+    return message.toUpperCase();
+  });
 };
 
-const handleResolvedOrRejectedPromise = (promise) => {
-};
+const handleResolvedOrRejectedPromise = (promise) => {};
 
-const pauseForMs = (ms) => {
-};
+const pauseForMs = (ms) => {};
 
 module.exports = {
   resolvedWrapper,
